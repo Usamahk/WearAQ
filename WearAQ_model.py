0ef6d36489a31bbb2ec2d40928efdc2229dc9078
Usamahk <usamah.khan@gmail.com>
2018-03-21 14:52:23 -0400

    simplified model to use on server

---
+++ WearAQ_model.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+import pandas as pd
+from pyKriging import kriging
+
+# =============================================================================
+# Pass through model
+# =============================================================================
+
+optimizer = 'ga'
+
+print('Setting up the Kriging Model')
+k = kriging(X_new, y_new)
+
+k.train(optimizer = optimizer)
+k.plot()
+
+new_loc = []
+
+# infill points to improve models accuracy
+for i in range(8):
+    newpoints = k.infill(1, method='error')
+    for point in newpoints:
+        k.addPoint(point, y[0])
+    k.train(optimizer=optimizer)
+    new_loc.append({'lon':point[0],'lat':point[1]})
+
+new_loc = pd.DataFrame(new_loc)
